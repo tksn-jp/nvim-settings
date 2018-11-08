dcc5422a54731f890f8e0112b625cf3d83118b45
tksn-jp <tk5nj9@gmail.com>
2018-11-08 21:51:19 +0900

    add gitignore

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./dein/installer.sh
 sleep 10s
-ln -sf ./.vimrc $HOME/.vimrc
+ln -sf $HOME/vim-settings/.vimrc $HOME/.vimrc
 if [ `which nvim` ]; then
 	if [ -d $HOME/.config/ ]; then
 		if [ -d $HOME/.config/nvim/ ]; then
-			ln -sf ./.vimrc $HOME/.config/nvim/init.vim
+			ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
 		else
 			mkdir $HOME/.config/nvim
-			ln -sf ./.vimrc $HOME/.config/nvim/init.vim
+			ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
 		fi
 	else
 		mkdir -p $HOME/.config/nvim
-		ln -sf ./.vimrc $HOME/.config/nvim/init.vim
+		ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
 	fi
 fi
 sh ./dein/installer.sh ./dein/
